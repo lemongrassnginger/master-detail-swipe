b587b76d1df40504a796dd6655a61e3dd2dc7ea4
Stuart Bell <stuart.bell@bluefinsolutions.com>
2017-11-30 10:48:37 +0000

    change indentation rules

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -10,30 +10,30 @@ sap.ui.define([
 	*/
 	return Controller.extend("mud.4ever.controller.Detail", {
 
-	// ********************************************************* //
-	// lifecycle
-	// ******************************************************* //
+		// ********************************************************* //
+		// lifecycle
+		// ******************************************************* //
 
-	/**
-	 * Runs on Controller "init" event
-	 * @return   {[type]}
- 	 * @memberof Detail
-	 */
-	onInit: function () {
-		// call base controller init
-		Controller.prototype.onInit.apply(this, arguments);
+		/**
+		* Runs on Controller "init" event
+		* @return   {[type]}
+		* @memberof Detail
+		*/
+		onInit: function () {
+			// call base controller init
+			Controller.prototype.onInit.apply(this, arguments);
 
-		// set the pages model as the default on the view
-		var oModel = this.getOwnerComponent().getModel("pages");
-		this.oView.setModel(oModel);
+			// set the pages model as the default on the view
+			var oModel = this.getOwnerComponent().getModel("pages");
+			this.oView.setModel(oModel);
 
-		// get carousel
-		var oCarousel = this.getView().byId("detail-carousel");
-		// get id from model
-		var sId = this.oView.getModel().getProperty("/pages")[1].id;
-		// add pages to the carousel
-		oCarousel.addPage(this.getXmlFragment(sId));
-	},
+			// get carousel
+			var oCarousel = this.getView().byId("detail-carousel");
+			// get id from model
+			var sId = this.oView.getModel().getProperty("/pages")[1].id;
+			// add pages to the carousel
+			oCarousel.addPage(this.getXmlFragment(sId));
+		}
 
 	});
 });
