544edc1d4dd8cb678f6fe82d9fb1d01777c4023a
Stuart Bell <stuart.bell@bluefinsolutions.com>
2017-11-30 13:49:40 +0000

    create page function

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -26,20 +26,39 @@ sap.ui.define([
 			// set the pages model as the default on the view
 			var oModel = this.getOwnerComponent().getModel("pages");
 			this.oView.setModel(oModel);
-
+			// page binding path
+			var sPageBindingPath = "/pages/1";
+			// get page data
+			var oPageData = this.oView.getModel().getProperty(sPageBindingPath);
+			// create page
+			var oPage = this.createPage(oPageData.id, sPageBindingPath);
 			// get carousel
 			var oCarousel = this.getView().byId("detail-carousel");
-			// get fragment and bind data
-			var oPageData = this.oView.getModel().getProperty("/pages")[0];
-			var oPage = this.getXmlFragment(oPageData.id);
+			// add pages to the carousel
+			oCarousel.addPage(oPage);
+		},
+
+		// ********************************************************* //
+		// private
+		// ******************************************************* //
+
+		/**
+		 * Creates a page control from a fragment and binds the view model
+		 * @param    {string} sFragmentName
+		 * @param    {string} sBindingPath
+		 * @return   {sap.m.Page}
+		 * @memberof Detail
+		 */
+		createPage: function (sFragmentName, sBindingPath) {
+			// get page ui from fragment
+			var oPage = this.getXmlFragment(sFragmentName);
 			// add the fragment as a dependent of the view so view model can be accessed
 			this.oView.addDependent(oPage);
 			// bind context of the page to the view model
-			oPage.bindElement({path: "/pages/0"});
-
-			// add pages to the carousel
-			oCarousel.addPage(oPage);
-		}
+			oPage.bindElement({path: sBindingPath});
+			// return the page control
+			return oPage;
+		},
 
 	});
 });
