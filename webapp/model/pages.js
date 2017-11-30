8e5488957e33356d2ad426a640ac9e46106b4330
Stuart Bell <stuart.bell@bluefinsolutions.com>
2017-11-30 10:16:48 +0000

    add pages model

---
+++ webapp/model/pages.js
@@ -0,0 +1,28 @@
+sap.ui.define([
+	"sap/ui/model/json/JSONModel"
+], function(JSONModel) {
+	"use strict";
+
+	/**
+	* @class pages
+	*/
+	return {
+
+		/**
+		* Initialise pages model
+		* @return   {[type]}
+		* @memberof pages
+		*/
+		createPagesModel: function() {
+			return new JSONModel({
+				pages: [
+					{
+						id: "test1",
+						text: "blubbblueblbublbhubebhelubhlebubluh"
+					}
+				]
+			});
+		}
+
+	};
+});
